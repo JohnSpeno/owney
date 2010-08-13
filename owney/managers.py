2428dcc620fae28e3f7f5ed268ff4bffb96c4501
John P. Speno <speno@macspeno.com>
2010-08-12 20:16:27 -0400

    Change filter syntax to be more direct.

--- owney/managers.py
+++ owney/managers.py
@@ -4,4 +4,4 @@ class ShipmentManager(Manager):
 """Returns Shipments that are not delivered""" 
        
     def undelivered(self):
-        return self.get_query_set().exclude(status__exact='delivered')
+        return self.get_query_set().exclude(status='delivered')
