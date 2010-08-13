0e313863fd96bcff1e19be5daa5f6c70b6ab2aa3
John P. Speno <speno@macspeno.com>
2010-08-12 20:13:13 -0400

    Formatting tweaks.

--- owney/views.py
+++ owney/views.py
@@ -6,14 +6,18 @@ from django.shortcuts import render_to_response
 USPS_URL = getattr(settings, 'TRACKING_USPS_URL')
 CS_URL = getattr(settings, 'TRACKING_CS_URL')
 
-def index(request, template_name="shipment_list.html"):
+def index(request, template_name='shipment_list.html'):
     """
     Display index of undelivered shipments by day.
     """
     shipments = Shipment.objects.undelivered()
     num_undelivered = shipments.count() 
-    return render_to_response(template_name,
-                                {'num_undelivered' : num_undelivered,
-                                 'ship_list' : shipments,
-                                 'usps_url' : USPS_URL,
-                                 'cs_url' : CS_URL})
+    return render_to_response(
+        template_name,
+        {
+            'num_undelivered' : num_undelivered,
+            'ship_list' : shipments,
+            'usps_url' : USPS_URL,
+            'cs_url' : CS_URL
+        }
+    )
