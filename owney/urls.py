d7008226918433939831dc8796a9f2fdff1c44a6
John P. Speno <speno@macspeno.com>
2010-08-12 20:19:30 -0400

    Remove admin. This should be in the project, not the app. I think.

--- owney/urls.py
+++ owney/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import *
 from models import Shipment
-from admin import *
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +7,5 @@ admin.autodiscover()
 import owney.views 
 
 urlpatterns = patterns('owney.views',
-    url(r'^admin/(.*)', admin.site.root), 
     url(r'^$', 'index', name="shipment_index"),
 )
